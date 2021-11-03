22c8e9294e17c72d9765b54a183a08aa11442350
Marcelo <marsleite@gmail.com>
2021-11-03 09:38:10 -0300

    Estilizando a aplicação com material ui

--- src/App.js
+++ src/App.js
@@ -9,12 +9,15 @@ import {
 
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles';
 
 const App = () => {
+  const classes = useStyles();
+
   return (
     <Container maxWidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">Tasks</Typography>
+      <AppBar className={ classes.appBar } position="static" color="inherit">
+        <Typography className={ classes.heading } variant="h2" align="center">Tasks</Typography>
       </AppBar>
       <Grow in>
         <Container>
